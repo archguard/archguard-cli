26af4352d71b9557ab2a7b8309a6449498532f1c
qinjialei <qinjialei@foxmail.com>
2020-09-23 21:29:44 +0800

    refactor

--- src/utils.js
+++ src/utils.js
@@ -2,33 +2,43 @@ const Handlebars = require('handlebars');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = {
-  toUpperCaseFirstWord(str) {
-    return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
-  },
-  generateFileByTemplate(template, data) {
-    const content = Handlebars.compile(template)(data);
-    return content;
-  },
-  travel(dir, callback) {
-    fs.readdirSync(dir).forEach(function (file) {
-      const pathname = path.join(dir, file);
+const fileObject = {};
 
-      if (fs.statSync(pathname).isDirectory()) {
-        travel(pathname, callback);
-      } else {
-        callback(pathname);
-      }
-    });
-  },
-  validate(conditions) {
-    let res = true;
-    for (const item of conditions) {
-      if (item.fn()) {
-        res = false;
-        console.error(item.message);
-      }
+function travel(dir, callback) {
+  fs.readdirSync(dir, { withFileTypes: true }).forEach(function (file) {
+    console.log('file: ', file);
+    const pathname = path.join(dir, file);
+    if (fs.statSync(pathname).isDirectory()) {
+      travel(pathname, callback);
+    } else {
+      callback(pathname);
     }
-    return (callback) => res && callback();
-  },
+  });
+}
+
+function toUpperCaseFirstWord(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function generateFileByTemplate(params) {
+  const content = Handlebars.compile(template)(data);
+  return content;
+}
+
+function validate(conditions) {
+  let res = true;
+  for (const item of conditions) {
+    if (item.fn()) {
+      res = false;
+      console.error(item.message);
+    }
+  }
+  return (callback) => res && callback();
+}
+
+module.exports = {
+  travel,
+  toUpperCaseFirstWord,
+  generateFileByTemplate,
+  validate,
 };
