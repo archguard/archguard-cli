9b713ae3dfd74304d7bdea85da2c6677eab0974e
qinjialei <qinjialei@foxmail.com>
2020-09-24 11:05:03 +0800

    feat: transform tsx to ast

--- src/commands/test.js
+++ src/commands/test.js
@@ -1,10 +1,28 @@
-const { travel } = require('../utils');
+const parser = require('@babel/parser');
+const fs = require('fs');
+const path = require('path');
+
+console.log('parser.parse 的值是：', parser.parse);
 
 function initCommandTest(program) {
-  program.command('test').action((options) => {
-    console.log('test');
-    // travel();
-  });
+  program
+    .command('test')
+    .alias('t')
+    .action((options) => {
+      fs.readFile(
+        path.join(process.cwd(), './layouts/base/config.tsx'),
+        (error, data) => {
+          const code = data.toString();
+          const ast = parser.parse(code, {
+            sourceType: 'module',
+            plugins: ['typescript', 'jsx'],
+          });
+          console.log('ast: ', ast);
+          // console.log('ast2: ', ast);
+        }
+      );
+      // travel();
+    });
 }
 
 module.exports = initCommandTest;
