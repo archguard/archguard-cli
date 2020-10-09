6876ffa87cac093b95f5ba528feb1a32e7181618
qinjialei <qinjialei@foxmail.com>
2020-10-09 20:46:09 +0800

    1

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // 以下路径均相对于 src 路径
 module.exports.MENU_PATH = './components/Business/Layouts/PageSider.tsx';
-module.exports.ROUTER_PATH = '../.umirc.ts';
+module.exports.ROUTER_PATH =  '../.umirc.ts';
